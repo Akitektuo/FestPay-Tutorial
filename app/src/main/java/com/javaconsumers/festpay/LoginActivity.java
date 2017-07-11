0abf2ec8018377606d4dfa19d207ea90612457fc
Akitektuo <alexcopindean@yahoo.com>
2017-07-11 17:08:22 +0300

    Login and Register activities are ready for implementing db

--- app/src/main/java/com/javaconsumers/festpay/LoginActivity.java
+++ app/src/main/java/com/javaconsumers/festpay/LoginActivity.java
@@ -5,45 +5,39 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText editUsername;
+    private EditText editEmail;
     private EditText editPassword;
+    private CheckBox checkRemember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        editUsername = (EditText) findViewById(R.id.edit_login_username);
-        editPassword = (EditText) findViewById(R.id.edit_login_password);
+        editEmail = (EditText) findViewById(R.id.edit_login_email);
+        editPassword = (EditText) findViewById(R.id.edit_register_password);
+        checkRemember = (CheckBox) findViewById(R.id.check_remember);
         Button buttonLogin = (Button) findViewById(R.id.button_login);
         Button buttonRegister = (Button) findViewById(R.id.button_register);
-        TextView textForgotPassword = (TextView) findViewById(R.id.text_forgot_password);
         buttonLogin.setOnClickListener(this);
         buttonRegister.setOnClickListener(this);
-        textForgotPassword.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.button_login) {
-            Toast.makeText(this, "Username: " + editUsername.getText().toString() + "\nPassword: " + editPassword.getText().toString(), Toast.LENGTH_LONG).show();
-            editUsername.setText("Username");
-        }
         switch (view.getId()) {
             case R.id.button_login:
-                Toast.makeText(this, "Username: " + editUsername.getText().toString() + "\nPassword: " + editPassword.getText().toString(), Toast.LENGTH_LONG).show();
+                //do check here
                 break;
             case R.id.button_register:
-                Intent intentRegister = new Intent(this, RegisterActivity.class);
-                startActivity(intentRegister);
-                break;
-            case R.id.text_forgot_password:
-                Toast.makeText(this, "Soon enough...", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(this, RegisterActivity.class));
+                editEmail.setText("");
+                editPassword.setText("");
+                checkRemember.setChecked(false);
                 break;
         }
     }
--- app/src/main/java/com/javaconsumers/festpay/RegisterActivity.java
+++ app/src/main/java/com/javaconsumers/festpay/RegisterActivity.java
@@ -2,12 +2,38 @@ package com.javaconsumers.festpay;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editEmail;
+    private EditText editEmailConfirm;
+    private EditText editPassword;
+    private EditText editPasswordConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        editEmail = (EditText) findViewById(R.id.edit_register_email);
+        editEmailConfirm = (EditText) findViewById(R.id.edit_register_email_confirm);
+        editPassword = (EditText) findViewById(R.id.edit_register_password);
+        editPasswordConfirm = (EditText) findViewById(R.id.edit_register_password_confirm);
+        findViewById(R.id.button_register_back).setOnClickListener(this);
+        findViewById(R.id.button_register_submit).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_register_back:
+                finish();
+                break;
+            case R.id.button_register_submit:
+                //add into db here
+                finish();
+                break;
+        }
     }
 }
